a2dd3b0ee051256880bf420d62fbc999d57d6e62
SC <github.com@genkisugi.net>
2016-03-14 13:49:58 -0700

    starting breakout of bbop-js bundled widgetry with new manager system; work on geneontology/amigo#317

---
+++ tests/trivial.test.js
@@ -0,0 +1,20 @@
+////
+//// This is just a test to test the testing environment.
+////
+
+// Prefer Chai assert. We're (somehow, it does not work for "assert")
+// passing in "should" from the gulp-defined globals.
+var assert = require('chai').assert;
+
+describe('our testing environment is sane', function(){
+    
+    // Can I pull in things as expected from node_modules, etc.?
+    it('I can see bbop-widget-set from my porch', function(){
+	var bbop_widgets = require('..');
+
+	var set_info = new bbop_widgets.set();
+	assert.typeOf(set_info, 'object');
+	assert.equal(set_info._is_a, 'bbop-widget-set', 'is_a');
+    });
+    
+});
